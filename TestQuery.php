d06480ad3865603826d2515c4417fdb33ce25561
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-07-06 14:01:42 +0700

    update TestQuery.php foreach pdo statement

--- TestQuery.php
+++ TestQuery.php
@@ -5,4 +5,16 @@
   $connection = getConnection();
   $sql = "SELECT *FROM customers";
   $statement = $connection->query($sql);
+
+  foreach($statement as $row)
+  {
+    $id = $row['id'];
+    $name = $row['name'];
+    $email = $row['email'];
+
+    echo "Id : $id" . PHP_EOL;
+    echo "Name : $name" . PHP_EOL;
+    echo "Email : $email" . PHP_EOL;
+  }
+
   $connection = null;
